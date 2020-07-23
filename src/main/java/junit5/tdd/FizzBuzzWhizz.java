6668e77db3fea03d280583356d7cc39cec38ba98
y24er <1028704486@qq.com>
2020-07-23 23:48:05 +0800

    main/[FizzBuzzWhizz][Jessie] feature: implement given 5 when say then buzz

--- src/main/java/junit5/tdd/FizzBuzzWhizz.java
+++ src/main/java/junit5/tdd/FizzBuzzWhizz.java
@@ -4,6 +4,8 @@ public class FizzBuzzWhizz {
     public String say(int number) {
         if (number % 3 == 0)
             return "Fizz";
+        if (number % 5 == 0)
+            return "Buzz";
         return String.valueOf(number);
     }
 }
