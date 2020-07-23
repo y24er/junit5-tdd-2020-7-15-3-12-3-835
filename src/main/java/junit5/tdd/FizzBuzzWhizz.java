c421a7248d0751cdbe49fad3294a5316953c95c4
y24er <1028704486@qq.com>
2020-07-23 23:49:47 +0800

    main/[FizzBuzzWhizz][Jessie] feature: implement given 7 when say then whizz

--- src/main/java/junit5/tdd/FizzBuzzWhizz.java
+++ src/main/java/junit5/tdd/FizzBuzzWhizz.java
@@ -6,6 +6,8 @@ public class FizzBuzzWhizz {
             return "Fizz";
         if (number % 5 == 0)
             return "Buzz";
+        if (number % 7 == 0)
+            return "Whizz";
         return String.valueOf(number);
     }
 }
