923c12c93ea5eb625763eddc049dddfc05ae3bb7
y24er <1028704486@qq.com>
2020-07-24 00:01:17 +0800

    [FizzBuzzWhizz][Jessie] refactor: refactor say method to avoid multiple return

--- src/main/java/junit5/tdd/FizzBuzzWhizz.java
+++ src/main/java/junit5/tdd/FizzBuzzWhizz.java
@@ -2,30 +2,14 @@ package junit5.tdd;
 
 public class FizzBuzzWhizz {
     public String say(int number) {
-        String result = null;
-        if (number % 3 == 0 && number % 5 == 0 && number % 7 == 0) {
-            result = "FizzBuzzWhizz";
-            return result;
-        }
-        if (number % 3 == 0 && number % 5 == 0) {
-            result = "FizzBuzz";
-            return result;
-        }
-        if (number % 3 == 0 && number % 7 == 0) {
-            result = "FizzWhizz";
-            return result;
-        }
-        if (number % 5 == 0 && number % 7 == 0) {
-            result = "BuzzWhizz";
-            return result;
-        }
+        String result = "";
         if (number % 3 == 0)
-            result = "Fizz";
+            result += "Fizz";
         if (number % 5 == 0)
-            result = "Buzz";
+            result += "Buzz";
         if (number % 7 == 0)
-            result = "Whizz";
-        if (result == null)
+            result += "Whizz";
+        if (result.equals(""))
             return String.valueOf(number);
         return result;
 
